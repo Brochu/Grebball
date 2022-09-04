ce97832bb7f8fd3aad3c104082fac69b811269ff
Alex Brochu <alexbrochu1@gmail.com>
2022-09-04 12:03:55 -0400

    Connected to mongodb, added couple of tests

--- GrebFlask/app.py
+++ GrebFlask/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, jsonify, request
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+from bson.json_util import dumps
+from pymongo import MongoClient
+
 import os
 import requests
 
@@ -8,14 +13,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/week', methods=['GET'])
-def getWeek():
-    season = request.args.get('season')
-    week = request.args.get('week')
-
-    if season is None or week is None:
-        return 'bad request', 400
-
+@app.route('/week/<season>/<week>', methods=['GET'])
+def getWeek(season, week):
     url = "https://www.thesportsdb.com/api/v1/json/2/eventsround.php"
 
     # Gets the results for a given season and week
@@ -39,6 +38,25 @@ def getWeek():
     req = requests.get(url = url, params = params)
     return req.json()
 
+@app.route('/poolers', methods=['GET'])
+def getPoolers():
+    client = MongoClient(os.environ.get('MONGO_URL'))
+
+    db = client.pool_football_app_dev
+    collection = db.poolers
+    alex = collection.find({ 'name': 'Alex' })[0]
+
+    coll = db.picks
+    picks = coll.find({ 'pooler_id': alex.get('_id') })
+
+    result = []
+    for p in picks:
+        result.append(p['pickstring'])
+
+    return result
+
 if __name__ == "__main__":
+    load_dotenv()
     port = int(os.environ.get('PORT', 5000))
+
     app.run(debug=True, host='0.0.0.0', port=port)

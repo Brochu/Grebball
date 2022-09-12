575f19e3e7e98b0aed99dde48572ee76b7208f44
Alex Brochu <alexbrochu1@gmail.com>
2022-09-12 10:49:27 -0400

    [Backend] Added first version of the picks index page

--- GrebFlask/picks.py
+++ GrebFlask/picks.py
@@ -3,12 +3,42 @@ from bson import ObjectId
 from flask import Blueprint, session, render_template, request
 
 from database import DB
-from football import GetWeek, GetTeamShortName, GetWeekLongName
+from football import GetWeek, GetTeamShortName, GetWeekLongName, GetWeekName
+from pools import FindCurrentWeek
 
 PicksBlueprint = Blueprint('picks_blueprint', __name__)
 
+MAX_WEEK = 22
 #TODO: Remove test user
 poolerid = ObjectId('5f70f0ffd8e2db255c9a0df6')
+
+@PicksBlueprint.route('/picks')
+def index():
+    #pooler = loads(session['pooler'])
+    pooler = DB.poolers.find({ '_id': poolerid })[0]
+    [season, _] = FindCurrentWeek()
+
+    seasonpicks = list(DB.picks.find(
+        filter = {
+        'pooler_id': pooler['_id'],
+        'season': season,
+        },
+        sort = [("week", 1)],
+    ))
+
+    picksmap = {}
+    for i in range(MAX_WEEK):
+        if i < len(seasonpicks):
+            picksmap[i+1] = seasonpicks[i]
+        else:
+            picksmap[i+1] = None
+
+    return render_template('picks.index.html',
+        GetWeekName = GetWeekName,
+        pooler = pooler,
+        season = season,
+        picksmap = picksmap,
+    )
 
 @PicksBlueprint.route('/picks/new/<strseason>/<strweek>')
 def new(strseason, strweek):
@@ -21,7 +51,7 @@ def new(strseason, strweek):
     pooler = DB.poolers.find({ '_id': poolerid })[0]
     pool = DB.pools.find({ '_id': pooler['pool_id'] })[0]
 
-    return render_template('picks.html',
+    return render_template('picks.new.html',
         GetTeamShortName = GetTeamShortName,
         GetWeekLongName = GetWeekLongName,
         season = season,

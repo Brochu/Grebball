8c390bd44f0badde2b4da057131f389d684b187d
Alex Brochu <alexbrochu1@gmail.com>
2022-10-10 12:08:38 -0400

    Added helper function to find latest week

--- GrebFlask/database.py
+++ GrebFlask/database.py
@@ -10,3 +10,33 @@ def GetPoolers():
         print(f' : {name} => {fav}')
 
     return {}
+
+def FindCurrentWeek():
+    seasonPipeline = [{
+        '$group': {
+            '_id': '$pooler_id',
+            'season_max': { '$max': '$season' },
+        }
+    }]
+
+    maxseason = 1900
+    for s in DB.picks.aggregate(seasonPipeline):
+        if s['season_max'] > maxseason:
+            maxseason = s['season_max']
+
+    weekPipeline = [{
+        '$match': { 'season': maxseason }
+    },
+    {
+        '$group': {
+            '_id': '$season',
+            'week_max': { '$max': '$week' },
+        }
+    }]
+
+    maxweek = 0
+    for s in DB.picks.aggregate(weekPipeline):
+        if s['week_max'] > maxweek:
+            maxweek = s['week_max']
+
+    return maxseason, maxweek

407d4c57e9f56edee1180384c66408e50c702e3d
Alex Brochu <alexbrochu1@gmail.com>
2022-10-22 02:39:35 -0400

    Fixed issue where, the wrong env variable was used as secret value

--- GrebReact/src/pages/api/auth/[...nextauth].js
+++ GrebReact/src/pages/api/auth/[...nextauth].js
@@ -5,9 +5,10 @@ const options = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
+    NEXTAUTH_URL: 'http://localhost:3000/api/auth/signin',
 }
 
 export default (req, res) => NextAuth(req, res, options)

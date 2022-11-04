c5f0c6205e3bceb9cd186647580182c9d590393a
Alex Brochu <alexbrochu1@gmail.com>
2022-11-03 23:58:31 -0400

    Fix issue that occurred when trying to build the docker image

--- GrebReact/src/__mocks__/pools.js
+++ GrebReact/src/__mocks__/pools.js
@@ -1,6 +1,6 @@
 export const pools = [
     {
-        _id: '5f5f04b4d8e2db1ba4200218'
+        _id: '5f5f04b4d8e2db1ba4200218',
         name: 'Brochus',
         motp: 'La reprise du pool historique du GREB de l\'Université Laval',
     },

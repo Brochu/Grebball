8c137f90a4d23f196e2e6242d63a13e520d49a38
Alexandre Brochu <alexbrochu1@gmail.com>
2022-11-20 16:17:57 +0000

    [SETUP] Changed backend off of debug mode

--- GrebFlask/app.py
+++ GrebFlask/app.py
@@ -62,4 +62,4 @@ if __name__ == '__main__':
     load_dotenv()
     port = int(os.environ.get('PORT', 5000))
 
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)

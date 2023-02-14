9df05749d9d9e3e15b41412233d5b982c736163c
Alex Brochu <alexbrochu1@gmail.com>
2023-02-13 23:16:22 -0500

    [DEV] Added helper function to query db for current week for a given pooler

--- GrebReact/src/lib/database.js
+++ GrebReact/src/lib/database.js
@@ -1,5 +1,7 @@
 import { MongoClient } from 'mongodb';
 
+// -------------------------------------------------
+// Database connection setup
 const uri = process.env.MONGO_URL;
 const options = {
     useUnifiedTopology: true,
@@ -18,6 +20,8 @@ clientPromise = client.connect();
 
 export default clientPromise;
 
+// -------------------------------------------------
+// Helper functions
 export async function FindPoolerByEmail(e) {
     const mongo = await clientPromise;
     const db = mongo.db(process.env.MONGO_DB_NAME);
@@ -27,4 +31,44 @@ export async function FindPoolerByEmail(e) {
 
     const poolers = await db.collection("poolers").find({ user_id: uid }).toArray();
     return poolers[0];
+}
+
+export async function FindCurrentWeekForPooler(pooler) {
+    const mongo = await clientPromise;
+    const db = mongo.db(process.env.MONGO_DB_NAME);
+
+    const seasonPipeline = [
+        {
+            '$match': {
+                'pooler_id': pooler['_id'],
+            }
+        },
+        {
+            '$group': {
+                '_id': '$pooler_id',
+                'season_max': { '$max': '$season' },
+            }
+        }
+    ];
+    const seasonAg = await db.collection("picks").aggregate(seasonPipeline).toArray();
+    const maxseason = seasonAg[0]["season_max"];
+
+    const weekPipeline = [
+        {
+            '$match': {
+                'pooler_id': pooler['_id'],
+                'season': maxseason,
+            }
+        },
+        {
+            '$group': {
+                '_id': '$pooler_id',
+                'week_max': { '$max': '$week' },
+            }
+        }
+    ];
+    const weekAg = await db.collection("picks").aggregate(weekPipeline).toArray();
+    const maxweek = weekAg[0]["week_max"];
+
+    return [maxseason, maxweek];
 }

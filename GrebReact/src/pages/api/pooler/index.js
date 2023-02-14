786c5931fadfdfde2946596012a271aeb6e6d295
Alex Brochu <alexbrochu1@gmail.com>
2023-02-13 23:15:56 -0500

    [FIX] Formatting for pooler api routes

--- GrebReact/src/pages/api/pooler/index.js
+++ GrebReact/src/pages/api/pooler/index.js
@@ -1,8 +1,11 @@
-import { FindPoolerByEmail } from '../../../lib/database'
+import { FindPoolerByEmail } from '../../../lib/database';
 
 export default async function handler(req, res) {
     const e = req.headers['pooler-email'];
     const pooler = await FindPoolerByEmail(e);
 
-    res.status(200).json(pooler);
+    res.status(200).json({
+        'name': pooler['name'],
+        'favTeam': pooler['favTeam'],
+    });
 }

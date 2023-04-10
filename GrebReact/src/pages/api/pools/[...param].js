8346aab26c335c2b2ad46125a2a1c19da247bbb3
Brochu <alexbrochu1@gmail.com>
2023-04-10 12:27:28 -0400

    Added stub for /pools/*season* and /pools/*season*/*week* routes in same file

---
+++ GrebReact/src/pages/api/pools/[...param].js
@@ -0,0 +1,9 @@
+import { GetWeek } from '../../../utils/football'
+
+export default async function handler(req, res) {
+    const { param } = req.query;
+    console.log(param);
+
+    //TODO: Implement the rest of this logic
+    res.status(200).json({ });
+}

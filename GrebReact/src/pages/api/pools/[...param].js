9b66da4c9af19db20449d3af673d263204c030c3
Brochu <alexbrochu1@gmail.com>
2023-04-11 13:20:07 -0400

    Added logic needed at the route level in API to handle full season results

--- GrebReact/src/pages/api/pools/[...param].js
+++ GrebReact/src/pages/api/pools/[...param].js
@@ -1,14 +1,23 @@
 import { FindPoolerByEmail } from '../../../lib/database'
-import { CreateWeekData } from '../../../utils/pools'
+import { CreateWeekData, CreateSeasonData } from '../../../utils/pools'
 
 export default async function handler(req, res) {
     const e = req.headers['pooler-email'];
     const pooler = await FindPoolerByEmail(e);
 
-    const [seasonstr, weekstr] = req.query['param'];
-    const season = Number(seasonstr);
-    const week = Number(weekstr);
+    if (req.query['param'].length === 1) {
+        const seasonstr = req.query['param'][0];
+        const season = Number(seasonstr);
 
-    const weekdata = await CreateWeekData(season, week, pooler);
-    res.status(200).json(weekdata);
+        const seasondata = await CreateSeasonData(season, pooler);
+        res.status(200).json(seasondata);
+    }
+    else if (req.query['param'].length === 2) {
+        const [seasonstr, weekstr] = req.query['param'];
+        const season = Number(seasonstr);
+        const week = Number(weekstr);
+
+        const weekdata = await CreateWeekData(season, week, pooler);
+        res.status(200).json(weekdata);
+    }
 }
